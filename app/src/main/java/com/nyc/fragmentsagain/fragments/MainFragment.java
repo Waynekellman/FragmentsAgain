1b11bdf7298398783edd38f3fabcdb5498f9fb80
Wayne Kellman <waynekellman@ac.c4q.nyc>
2017-11-19 17:44:33 -0500

    first fragment

--- app/src/main/java/com/nyc/fragmentsagain/fragments/MainFragment.java
+++ app/src/main/java/com/nyc/fragmentsagain/fragments/MainFragment.java
@@ -3,6 +3,8 @@ package com.nyc.fragmentsagain.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +33,20 @@ public class MainFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_main, container, false);
         Button button = (Button) rootView.findViewById(R.id.button);
         editText = (EditText) rootView.findViewById(R.id.edit_text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentOne fragmentOne = new FragmentOne();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Bundle bundle = new Bundle();
+                String textFromEdit = editText.getText().toString();
+                bundle.putString("one",textFromEdit);
+                fragmentOne.setArguments(bundle);
+                fragmentTransaction.replace(R.id.main_container, fragmentOne);
+                fragmentTransaction.commit();
+            }
+        });
         return rootView;
     }
 
